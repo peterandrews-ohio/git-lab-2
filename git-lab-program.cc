1c3e39aeae55d7a0f0a6b40bcbf12ecde5c6d45d
peterandrews-ohio <pa297810@ohio.edu>
2022-01-21 15:08:21 -0500

    Added C++ file

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Peter Andrews
+ *        Date: 1/21/2022
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
